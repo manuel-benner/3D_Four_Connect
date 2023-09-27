4d7f004eb1d944d1f49f625ad3a733134c169a9d
Haeringer <Robin.Haeringer@rena.com>
2023-09-27 09:00:37 +0200

    Transparente Baelle ueber den Roehren

---
+++ Assets/Scripts/KugelAuswahlFarbeAendern.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KugelAuswahlFarbeAendern : MonoBehaviour
+{
+    public Material unhoveredMat;
+    public Material hoveredMatPlayer1;
+    public Material hoveredMatPlayer2;
+    public GameObject prefabToSpawnPlayer1;
+    public GameObject prefabToSpawnPlayer2;
+    // false = player1 turn, true = player2 turn
+    public bool player;
+
+    private bool isHovered = false;
+    private Material currentMaterial;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rend = GetComponent<Renderer>();
+        currentMaterial = new Material(unhoveredMat);
+        rend.material = currentMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Swap to opaque material if being hovered
+    private void OnMouseEnter()
+    {
+        if (!isHovered)
+        {
+            isHovered = true;
+            if (player)
+            {
+                currentMaterial = new Material(hoveredMatPlayer1);
+            }
+            else
+            {
+                currentMaterial = new Material(hoveredMatPlayer2);
+            }
+            rend.material = currentMaterial;
+        }
+    }
+
+    // Swap back to transparent material if not being hovered
+    private void OnMouseExit()
+    {
+        if (isHovered)
+        {
+            isHovered = false;
+            currentMaterial = new Material(unhoveredMat);
+            rend.material = currentMaterial;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (player)
+        {
+            Instantiate(prefabToSpawnPlayer1, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(prefabToSpawnPlayer2, transform.position, Quaternion.identity);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Spielfeld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spielfeld : MonoBehaviour
+{
+    public bool playerTurn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTurn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
