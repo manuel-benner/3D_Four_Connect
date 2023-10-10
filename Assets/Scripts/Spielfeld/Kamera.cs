db7fed51d80d7e2b9095ee7b634076cb6a9a28e8
Haeringer <Robin.Haeringer@rena.com>
2023-10-10 12:39:17 +0200

    Kamera bewegung veraendert

--- Assets/Scripts/Spielfeld/Kamera.cs
+++ Assets/Scripts/Spielfeld/Kamera.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class Kamera : MonoBehaviour
 {
+    float inputFactor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputFactor = 150.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float yChange;
+        float zChange;
+        float angleChange;
         // C -> Kamera vom Betrachter weg
         // V -> Kamera zum Betrachter hin
         if (Input.GetKey(KeyCode.C))
         {
             if(!(transform.position.y < 33))
             {
-                transform.Translate(0, -0.1f, 0.4f);
+                yChange = -0.7f * inputFactor * Time.deltaTime;
+                zChange = 0.4f * inputFactor * Time.deltaTime;
+                transform.Translate(0, yChange, zChange);
                 var currEulerAngles = transform.eulerAngles;
                 currEulerAngles.x -= 0.08f;
                 transform.rotation = Quaternion.Euler(currEulerAngles);
@@ -28,24 +34,34 @@ public class Kamera : MonoBehaviour
         
         else if (Input.GetKey(KeyCode.V))
         {
-            if(!(transform.position.y > 200))
+            if(!(transform.position.y > 175))
             {
-                transform.Translate(0, 0.1f, -0.4f);
+                yChange = 0.7f * inputFactor * Time.deltaTime;
+                zChange = -0.4f * inputFactor * Time.deltaTime;
+                transform.Translate(0, yChange, zChange);
                 var currEulerAngles = transform.eulerAngles;
                 currEulerAngles.x += 0.08f;
                 transform.rotation = Quaternion.Euler(currEulerAngles);
             }            
         }
-        
+
 
         // J -> Kamera nach links um das Spielfeld herum
         // K -> Kamera nach rechts um das Spielfeld herum
         if (Input.GetKey(KeyCode.J))
+        {
+            angleChange = 0.5f * inputFactor * Time.deltaTime;
             transform.RotateAround(new Vector3(0, 0, 0),
-            new Vector3(0, 1, 0), 0.25f);
+            new Vector3(0, 1, 0), angleChange);
+        }
+
         else if (Input.GetKey(KeyCode.K))
+        {
+            angleChange = -0.5f * inputFactor * Time.deltaTime;
             transform.RotateAround(new Vector3(0, 0, 0),
-            new Vector3(0, 1, 0), -0.25f);
+            new Vector3(0, 1, 0), angleChange);
+        }
+
 
     }
 }
